c95bcc7b345fb7a0faa52e4e9b9da455d3501893
Lmckim <lmckim.dev@gmail.com>
2019-03-21 03:45:06 -0400

    image resizing, card buildout, card titling

--- assets/php/classes/pageBuild.php
+++ assets/php/classes/pageBuild.php
@@ -2,12 +2,16 @@
 
 class pageBuild{
 
-    public function buildPage($mageDir)
+    public function buildPage($imageDir)
     {
+        $card = 'card.html';
+        $cardTemplate = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/pages/elements/'.$card);
+        $imageDir = array_diff(scandir($imageDir),array('..','.'));
+        array_splice($imageDir,sizeof($imageDir)-1);
         $page ='';
         $page .= $this->addHeader();
         $page .= $this->addNav();
-        $page .= $this->generateCards($imageDir);
+        $page .= $this->generateCards($imageDir,$cardTemplate);
         $page .= $this->addFooter();
         return $page;
     }
@@ -22,9 +26,20 @@ class pageBuild{
         $nav = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/pages/nav-bar.html');
         return $nav;
     }
-    private  function generateCards($imageDir)
-    {
-    
+    private function generateCards($imageDir,$cardTemplate)
+    {   
+        $content = '';
+        foreach($imageDir as $image)
+        {
+            $newCard = $cardTemplate;
+            $imageTitle = substr($image,0,strpos($image,"."));
+            $this->insertString_replaceKey($newCard,$imageTitle,'{{title}}');
+            $class = 'card-image';
+            $imagePath = '../../images/thumbs/' . $image;
+            $this->insertImageSource_atClass($newCard,$imagePath,$class);
+            $content .= $newCard;
+
+        }
         return $content;
 
     }
@@ -32,6 +47,21 @@ class pageBuild{
     {
         $footer = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/pages/footer.html');
         return $footer;
+    }
+
+    private function insertString_replaceKey(&$baseString,$stringToInsert,$insertionKey)
+    {
+        $offset = strlen($insertionKey);
+        $insertionPoint = strpos($baseString,$insertionKey);
+        $baseString = substr_replace($baseString,$stringToInsert,$insertionPoint,$offset);
+    }
+    private function insertImageSource_atClass(&$baseString,$imageSrc,$class)
+    {
+        $stringToFind = 'class=' . $class;
+        $classPos = strpos($baseString,$stringToFind);
+        $srcPos = strpos($baseString,"src=\"\"",$classPos);
+        $insertionPoint = $srcPos + strlen("src=\"");
+        $baseString = substr_replace($baseString,$imageSrc,$insertionPoint,0);
     }
 }
 ?>
