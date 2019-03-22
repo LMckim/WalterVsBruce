505e1ca5821af0eb00a4495c2decbe27bdad2291
Lmckim <lmckim.dev@gmail.com>
2019-03-22 17:51:24 -0400

    insert-user now working

--- constructors/insert-user.php
+++ constructors/insert-user.php
@@ -7,28 +7,28 @@ print('please confirm password: ');
 $pass2 = readline();
 if($upass != $pass2)
 {
-    print('passwords do not match, exiting now...');
+    print("passwords do not match, exiting now...\n");
     exit();
 }else{
     $conn = new mysqli('localhost','WalterVsBruce','BjsCiWtZgDIcsa84','WalterVsBruce');
-    file_get_contents($_SERVER['DOCUMENT_ROOT'].'../assets/php/security/PBKDF2.php');  
+    include_once($_SERVER['DOCUMENT_ROOT'].'../assets/php/security/PBKDF2.php');  
 
     $sec = new PasswordStorage();
     $pass = $sec->create_hash($upass);
-    $sql = "SELECT * FROM `users` WHERE `user_name`='$uname'";
+    $sql = "SELECT * FROM `users` WHERE `uName`='$uname'";
     $result = $conn->query($sql);
     if(mysqli_num_rows($result) != 0)
     {
-        print('username taken already, exiting now...');
+        print("username taken already, exiting now...\n");
         exit();
     }else{
         $sql = "INSERT INTO `users` (`uName`,`uPass`,`stay`) VALUES('$uname','$pass','N')";
         if($conn->query($sql))
         {
-            print('user successfully created, exiting now...');
+            print("user successfully created, exiting now...\n");
             exit();
         }else{
-            print('error creating new user, exiting now...');
+            print("error creating new user, exiting now...\n");
             exit();
         }
     }
