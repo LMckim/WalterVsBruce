7b96c7c45216ebc5962866c3ce031069a58b4c7a
Lmckim <lmckim.dev@gmail.com>
2019-03-24 18:15:37 -0400

    db update

--- database/tables-images.sql
+++ database/tables-images.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 22, 2019 at 03:38 PM
+-- Generation Time: Mar 24, 2019 at 06:12 PM
 -- Server version: 5.7.25-0ubuntu0.18.04.2
 -- PHP Version: 7.2.15-0ubuntu0.18.04.1
 
@@ -17,16 +17,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `img_id` int(11) NOT NULL,
+  `user` varchar(256) NOT NULL,
+  `comment` varchar(2048) NOT NULL,
+  `time_commented` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `images`
 --
 
 CREATE TABLE `images` (
-  `img_id` int(11) NOT NULL,
-  `fileName` varchar(256) NOT NULL,
-  `filePath` varchar(256) NOT NULL,
+  `id` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `path` varchar(256) NOT NULL,
   `latitude` varchar(25) NOT NULL,
   `longitude` varchar(25) NOT NULL,
-  `timeTaken` datetime NOT NULL,
+  `date` varchar(128) NOT NULL,
   `timeUpload` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -44,14 +58,29 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`u_id`, `uName`, `uPass`, `stay`) VALUES
+(1, 'luke', 'sha1:64000:18:lbdRiPwfNksaq+YB8aSwkmb8Dqa9OqIs:kjql/u9tIaxZY2rTS8HYFrqJ', 'N'),
+(2, 'waltd', 'sha1:64000:18:LrOUiMALzcF8RXZyvSfj3GQmD4EYxDi6:KBGdP424yIDdvz0xUJBZAATL', 'N');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `img_id` (`img_id`);
 
 --
 -- Indexes for table `images`
 --
 ALTER TABLE `images`
-  ADD PRIMARY KEY (`img_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -64,12 +93,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `img_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `image_relation` FOREIGN KEY (`img_id`) REFERENCES `images` (`id`);
