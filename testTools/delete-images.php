e34f9866e43510949bcbe0ea78022d9e39b7d7f4
Lmckim <lmckim.dev@gmail.com>
2019-03-25 03:43:00 -0400

    testTools  DB integration | minor css change

--- testTools/delete-images.php
+++ testTools/delete-images.php
@@ -1,4 +1,5 @@
 <?php
+$conn = new mysqli('localhost','WalterVsBruce','BjsCiWtZgDIcsa84','WalterVsBruce');
 $root = '/d_drive/Development/repos/WalterHikesBruce';
 include_once($root.'/assets/php/helpers/directoryTools.php');
 
@@ -8,6 +9,20 @@ $dir2 = $root.'/images/upload';
 $thumbs = parseDirectory_forFiles($dir1);
 $imgs = parseDirectory_forFiles($dir2);
 
+// remove image references from DB then recreate foreign key
+$sqlQuerys = array( "TRUNCATE `comments`",
+                    "ALTER TABLE `comments` DROP FOREIGN KEY `image_relation`",
+                    "TRUNCATE `images`",
+                    "ALTER TABLE `comments` ADD CONSTRAINT `image_relation` FOREIGN KEY (`img_id`) REFERENCES `images`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
+foreach($sqlQuerys as $query)
+{
+    $result = $conn->query($query);
+    if($result != 1)
+    {
+        print('error: could not execute query: '.$query. 'exiting program...');
+        exit();
+    }
+}
 foreach($thumbs as $img)
 {
     unlink($dir1 .'/'. $img);
@@ -16,5 +31,5 @@ foreach($imgs as $img)
 {
     unlink($dir2 .'/'. $img);
 }
-print('all images files removed'."\n");
+print('all images files removed, database rebuilt'."\n");
 ?>
